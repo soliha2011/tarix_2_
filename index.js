a61d988c292c66849ed08408cb52c9095fc8e39c
soliha2011 <zaynitdinovanogora@gmail.com>
2023-09-24 22:48:41 -0700

    ok

---
+++ index.js
@@ -0,0 +1,15 @@
+let ok1 = prompt()
+let ok2 =  prompt()
+let ok3 = prompt()
+
+let dollr = '11000'
+
+let yevro = '12300'
+
+
+console.log( ok1 * dollr)
+
+console.log( ok2  * yevro)
+
+console.log( ok3 * dollr)
+
